4e08aa8d129ba59502e00ea23c694c6d4d2a49a0
cynthia <cynthiaawsajira@gmail.com>
2025-02-13 02:31:44 +0300

    feat: add Flask application with Docker configuration
    - Add Flask app (app.py) with a simple endpoint.
    - Define Dockerfile to build Flask container (Python alpine image).
    - Include requirements.txt for Flask dependency.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template_string
 from datetime import datetime
 
 app = Flask("app")
 
 def gethtml():
-    return '<html><body><h1>I am a python flask web application!</h1><div>' + str(datetime.now()) + '</div></body></html>'
+    html_content = '''
+    <html>
+        <body>
+            <h1>Hey there! I'm a Python Flask web app!</h1>
+            <div>Current time: {{ current_time }}</div>
+            <div>🎉Surprise!🎉 You just got served🛎... by an Nginx reverse proxy🔀, not the Flask app directly😁</div>
+        </body>
+    </html>
+    '''
+    return render_template_string(html_content, current_time=datetime.now())
 
 @app.route("/")
 def index():
